0c4d886cc714f0674cb195358525da3a762cf7f5
Tadas Ivanauskas <ivanauskas15@gmail.com>
2017-07-07 10:03:35 +0200

    Add files via upload

---
+++ Launcher CSharp/RealmListChanger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Launcher
+{
+    class RealmListChanger
+    {
+        private string realmlist;
+        public RealmListChanger(string realm)
+        {
+            this.realmlist = realm;
+        }
+
+
+        public bool save()
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("C:/Users/ivana/Downloads/WoW TBC/WoW TBC/realmlist.wtf"))
+                {
+                    sw.WriteLine(realmlist);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(realmlist);
+                return false;
+            }
+        }
+    }
+}
